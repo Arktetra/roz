ddd9bd8a15d77a93bb900fb9d31edc0017debb6c
Arktetra <sameershrestha.sm@gmail.com>
2024-09-13 20:38:39 +0545

    remove flag for ast

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,24 @@ use std::{
     process::ExitCode
 };
 
-use roz::Flag;
-
 pub mod literal;
 pub mod lexer;
-pub mod expr;
 pub mod parser;
 pub mod interpreter;
+pub mod stmt;
 pub mod roz;
 
 fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
     
     if args.len() == 1 {
-        roz::run_prompt(Flag::Run);
+        roz::run_prompt();
     } else if args.len() == 2 {
-        if args[1] == "--ast" {
-            roz::run_prompt(Flag::Ast);
-        } else {
             return roz::run_file(&args[1]);
-        }
     } else {
         if args.len() > 3 {
             writeln!(io::stderr(), "Usage: {}", args[0]).unwrap();
             writeln!(io::stderr(), "Usage: {} <filename>", args[0]).unwrap();
-            writeln!(io::stderr(), "Usage: {} ast", args[0]).unwrap();
         }
     }
     
