16bbf602dfff80a08d4a014aa418655331bbe4f6
Arktetra <sameershrestha.sm@gmail.com>
2024-09-13 09:53:08 +0545

    add: REPL

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,44 @@
-use std::env;
-use std::fs;
-use std::io::{self, Write};
-use std::process::ExitCode;
+use std::{
+    env,
+    fs,
+    io::{self, Write},
+    process::ExitCode
+};
+
+use roz::Flag;
 
 pub mod literal;
 pub mod lexer;
 pub mod expr;
 pub mod parser;
 pub mod interpreter;
-
-use lexer::Lexer;
-
+pub mod roz;
 
 fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
-
-    if args.len() < 3 {
-        writeln!(io::stderr(), "Usage: {} tokenize <filename>", args[0]).unwrap();
-        return ExitCode::from(1);
-    }
-
-    let command = &args[1];
-    let filename = &args[2];
-
-    match command.as_str() {
-        "tokenize" => {
-
+    
+    if args.len() == 1 {
+        roz::run_prompt(Flag::Run);
+    } else if args.len() == 2 {
+        if args[1] == "--ast" {
+            roz::run_prompt(Flag::Ast);
+        } else {
+            let filename = &args[1];
             let filecontent = fs::read_to_string(filename)
                 .unwrap_or_else(|_| {
                     writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
                     String::new()
-            });
-
-            let mut lexer = Lexer::new(&filecontent);
-
-            if !filecontent.is_empty() {
-                // panic!("Scanner not implemented");
-                lexer.scan_tokens();
-
-                for token in lexer.tokens {
-                    println!("{}", token.to_string());
-                }
-
-                if lexer.had_error {
-                    return ExitCode::from(65);
-                }
-
-            } else {
-                println!("EOF null");
-            }
+                });
+    
+            println!("{}", roz::run(&filecontent));
         }
-        _ => {
-            writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
-            return ExitCode::from(1);
+    } else {
+        if args.len() > 3 {
+            writeln!(io::stderr(), "Usage: {}", args[0]).unwrap();
+            writeln!(io::stderr(), "Usage: {} <filename>", args[0]).unwrap();
+            writeln!(io::stderr(), "Usage: {} ast", args[0]).unwrap();
         }
     }
+    
     ExitCode::SUCCESS
 }
