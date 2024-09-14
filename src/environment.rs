d441d6b133536ee3165afd889e67199b43f88c62
Arktetra <sameershrestha.sm@gmail.com>
2024-09-14 17:07:27 +0545

    update environment to support scopes and shadowing

--- src/environment.rs
+++ src/environment.rs
@@ -4,25 +4,35 @@ use crate::lexer::Token;
 use crate::literal::Literal;
 use crate::interpreter::RuntimeError;
 
+#[derive(Clone)]
 pub struct Environment {
-    values: HashMap<String, Literal>
+    values: HashMap<String, Literal>,
+    enclosing: Box<Option<Environment>>
 }
 
 impl Environment {
-    pub fn new() -> Self {
-        Environment { values: HashMap::new() }
+    pub fn new(enclosing: Option<Environment>) -> Self {
+        Environment { 
+            values: HashMap::new(),
+            enclosing: Box::new(enclosing)
+        }
     }
 
     pub fn define(&mut self, name: String, value: Literal) {
         self.values.insert(name, value);
     }
 
-    pub fn get(&mut self, name: Token) -> Result<Literal, RuntimeError> {
+    pub fn get(&self, name: Token) -> Result<Literal, RuntimeError> {
         if let Some(value) = self.values.get(&name.lexeme) {
             Ok(value.clone())
         } else {
-            let message = format!("undefined variable name'{}'", name.lexeme);
-            Err(RuntimeError{token: name, message: message})
+            match *self.enclosing.clone() {
+                Some(enclosing) => enclosing.get(name),
+                None => {
+                    let message = format!("undefined variable '{}'", name.lexeme);
+                    Err(RuntimeError{token: name, message: message})
+                }
+            }
         }
     }
 
@@ -31,8 +41,16 @@ impl Environment {
             self.values.insert(name.lexeme, value);
             Ok(())
         } else {
-            let message = format!("undefined variable '{}'", name.lexeme);
-            Err(RuntimeError { token: name, message })
+            match *self.enclosing.clone() {
+                Some(enclosing) => {
+                    self.values.insert(name.lexeme.clone(), enclosing.get(name)?);
+                    Ok(())
+                },
+                None => {
+                    let message = format!("undefined variable '{}'", name.lexeme);
+                    Err(RuntimeError{ token: name, message })
+                }
+            }
         }
     }
 }
