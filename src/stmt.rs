904e6d971a1ce4a710a4d9e2d4f090510bb990f8
Arktetra <sameershrestha.sm@gmail.com>
2024-09-15 19:54:45 +0545

    add: call expression and function statement

--- src/stmt.rs
+++ src/stmt.rs
@@ -1,24 +1,35 @@
 use crate::{lexer::Token, literal::Literal};
 
-#[derive(PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Expr {
-    Logical(Box<Expr>, Token, Box<Expr>),
-    Binary(Box<Expr>, Token, Box<Expr>),
-    Unary(Token, Box<Expr>),
-    Grouping(Box<Expr>),
-    Literal(Literal),
-    Variable(Token),
-    Assign(Token, Box<Expr>),
+    Logical(Box<Expr>, Token, Box<Expr>),   // left operand, operator, right operand
+    Binary(Box<Expr>, Token, Box<Expr>),    // left operand, operator, right operand
+    Unary(Token, Box<Expr>),                // operator, operand
+    Grouping(Box<Expr>),                    // (expression)
+    Literal(Literal),                   
+    Variable(Token),                        // name
+    Assign(Token, Box<Expr>),               // name, value
+    Call(Box<Expr>, Token, Vec<Expr>),      // callee, paren, list of argument
+    None    
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Stmt {
+    Expression(Expr),                       // expression
+    If(Expr, Box<Stmt>, Box<Stmt>),         // condition, then branch, else branch
+    While(Expr, Box<Stmt>),                 // condition, body
+    Function(Token, Vec<Token>, Box<Stmt>), // name, params, body
+    Print(Expr),                            // expression
+    Var(Token, Expr),                       // name, initializer
+    Block(Vec<Stmt>),                       // list of statement
     None
 }
 
-#[derive(PartialEq)]
-pub enum Stmt {
-    Expression(Expr),
-    If(Expr, Box<Stmt>, Box<Stmt>),
-    While(Expr, Box<Stmt>),
-    Print(Expr),
-    Var(Token, Expr),
-    Block(Vec<Stmt>),
-    None
+impl Stmt {
+    pub fn get_block_body(&self) -> Option<&Vec<Stmt>> {
+        match self {
+            Stmt::Block(stmts) => Some(stmts),
+            _ => None
+        }
+    }
 }
