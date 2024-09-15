61c9fa83367c4d6674a1c484fbef493998a2b4d5
Arktetra <sameershrestha.sm@gmail.com>
2024-09-15 19:42:53 +0545

    add callable trait

---
+++ src/callable.rs
@@ -0,0 +1,6 @@
+use crate::{interpreter::Interpreter, literal::Literal};
+
+pub trait Callable {
+    fn arity(&self) -> usize;
+    fn call(&self, interpreter: &mut Interpreter, arguments: Vec<Literal>) -> ();
+}
